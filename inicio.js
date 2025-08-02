f83ba504525d87cf3e2bb36aef7396e85dfdbad4
santiagoacosta123 <santiagobarrera2004s@gmail.com>
2025-08-02 11:01:34 -0500

    subiendo archivos

---
+++ inicio.js
@@ -0,0 +1,15 @@
+var numero = 5;
+console.log("El valor del número es: " + numero);
+
+let nombre = "Santiago Acosta"; 
+var edad = 21;
+var hijos = 2;
+var sueldobasico = 5000; 
+
+console.log("Datos personales:");
+console.log("Nombre: " + nombre);
+console.log("Edad: " + edad + " años");
+console.log("Hijos: " + hijos);
+console.log("Sueldo básico: $" + sueldobasico);
+let masculino
+console.log("Mi genero es masculino");
